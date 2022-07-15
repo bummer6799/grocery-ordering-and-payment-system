2042f8b53940a713593cfab0ad4f15eb7fbf7f11
bummer6799 <rojapoo.rs@gmail.com>
2022-07-15 14:23:44 +0800

    Replaced the list with dictionary

--- main.py
+++ main.py
@@ -1,15 +1,10 @@
 import math
 
 
-Dairy = ["Milk", "Butter", "Eggs", "Cheese slices", "Evaporated Milk Creamer", "Milo", "Biscuits", "Yogurt"]
-# DairyPrices = ["2.30", "4.50", "3.40", "3.15", "1.40", "12.50", "5.30", "0.95"]
-DairyPrices = [2.30, 4.50, 3.40, 3.15, 1.40, 12.50, 5.30, 0.95]
-DairyPricesGST = [i * 107/100 for i in DairyPrices]
-for i in DairyPricesGST:
-    print("%.2f" % DairyPricesGST)
-print(*Dairy + DairyPricesGST, sep='\n')
+dairy = {'Milk':2.3, 'Butter':4.5, 'Eggs':3.4, 'Cheese Slices':3.15, 'Evaporated Milk Creamer':1.4, 'Milo':12.4, 'Biscuits':5.3, 'Yogurt':0.95}
 
+UserInput = input("1 - Dairy \nEnter Category: ")
 
-
-UserInput = int(input("Selection:"))
-print(Dairy[UserInput], DairyPricesGST[UserInput])
+if UserInput == "1":
+    for key in dairy:
+        print(key,dairy[key])
